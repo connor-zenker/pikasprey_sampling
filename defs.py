03a74325513c213b7494fd39ba6f28a66a861ba2
connor-zenker <connorzenker@gmail.com>
2024-08-16 14:23:13 -0400

    Update defs.py

--- defs.py
+++ defs.py
@@ -1,9 +1,6 @@
 import numpy as np
 
 def f(n):
-    maxD = 0
-    for i in range(0,1000):
-        draw = np.random.binomial(231, 0.25)
-    if draw > maxD:
-        maxD = draw
+    draw = np.random.binomial(231, 0.25, 1000)
+    maxD = max(draw)
     return maxD
